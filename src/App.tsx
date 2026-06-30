d70e823ba40a286de911f41a174cbc47ee916ef3
Arturo Martinez <arturo.jms.prog@gmail.com>
2026-06-30 12:34:58 -0600

    * feat(wip): working on post router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Post  from './components/Post.tsx'
 import Sidebar from './components/Sidebar.tsx'
-import { PostService } from './services/postService.ts'
-import type { PostObject } from './interfaces/post.ts'
+import { Outlet, Route, Routes } from 'react-router'
 
 
 
 function App() {
-
-  let [content, setContent] = useState<PostObject>()
-  useEffect(() => {
-      const getContent = async () => {
-        const postContent = PostService("welcome");
-        const data = await postContent;
-        setContent(data);
-      }
-      getContent();
-  },[]);
 
   return (
     <>
       <div className="page">
         <Sidebar latestPosts={["Welcome to my page!"]}/>
         <div className='content'>
-          <Post title={content ? content.post.title : "Upss!! I did not find that ( ˶°ㅁ°) !!"} sections={content ? content.post.sections : []}/>
+          <Routes>
+            <Route index element={<Post post="welcome"/>}/>
+            <Route path='posts/:postName' element={<Post/>}/>
+          </Routes>
         </div>
       </div>
+      <Outlet/>
     </>
   )
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,32 @@
 import './Post.css'
-import type { PostDetails } from '../interfaces/post'
+import type { PostObject } from '../interfaces/post'
+import { useParams } from 'react-router'
+import { useEffect, useState } from 'react';
+import { PostService } from '../services/postService';
 
+// "Upss!! I did not find that ( ˶°ㅁ°) !!"
 
+export default function Post(optionalPostName: {post?: string}) {
 
-export default function Post(content: PostDetails) {
+    const params = useParams<{postName: string}>();
+    let [content, setContent] = useState<PostObject>();
+
+    useEffect(() => {
+        const postToRetrieve = optionalPostName.post ? optionalPostName.post : params.postName;
+        const getContent = async () => {
+            const postContent = PostService(postToRetrieve? postToRetrieve : "");
+            const data = await postContent;
+            setContent(data);
+        }
+        getContent();
+    },[]);
+
     return (
         <>
             <div className='post-content'>
-                <h1>{content.title}</h1>
+                <h1>{content?.post.title}</h1>
                 {
-                    content.sections.map(s => (
+                    content?.post.sections.map(s => (
                         <div key={s.title}>
                             <h2>{s.title}</h2>
                             <p>{s.text}</p>
