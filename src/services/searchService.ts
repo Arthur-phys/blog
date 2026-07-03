3a5a1d759bf631fb13dcc8b9ba000c9646bf5c98
Arturo Martinez <arturo.jms.prog@gmail.com>
2026-07-03 15:49:10 -0600

    * fix: minor update to wor in progress in welcome page

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,6 +1,6 @@
-import type { PostIndex, PostOverview } from "../interfaces/post";
+// import type { PostIndex, PostOverview } from "../interfaces/post";
 
-export function CreateSearchFunc(searchArr: PostIndex, returnLimit: number): (post: string, foundPosts: PostOverview[]) => PostOverview[] {
-    function Search(post: string, foundPosts: PostOverview[]): PostOverview[] {
+// export function CreateSearchFunc(searchArr: PostIndex, returnLimit: number): (post: string, foundPosts: PostOverview[]) => PostOverview[] {
+//     function Search(post: string, foundPosts: PostOverview[]): PostOverview[] {
         
-    } 
+//     } 
